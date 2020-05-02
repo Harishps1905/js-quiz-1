03556073e3bbb61636d182830e4ff6d192743d2e
harish <harishswami62@gmail.com>
2020-05-02 21:58:07 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,35 @@
+check = () => {
+    var qus1 = document.quiz.question1.value;
+    var qus2 = document.quiz.question2.value;
+    var qus3 = document.quiz.question3.value;
+    var correct = 0;
+
+    if (qus1 == "Delhi") {
+        correct++;
+    }
+    if (qus2 == "Mahathma Gandhi") {
+        correct++;
+    }
+    if (qus3 == "1947") {
+        correct++;
+    }
+
+    var messages = ["Good job DOOD", "Thats ok man", "You really need to work on it  DOOD"];
+    var pictures = ["../images/winner.gif", "../images/notbad.gif", "../images/lose.gif"];
+    var range = 0;
+
+    if (correct < 1) {
+        range = 2;        
+    }
+    if ((correct > 0) && (correct < 3)) {
+        range = 1
+    }
+    if (correct > 2) {
+        range = 0;
+    }
+
+    document.getElementById("message").innerHTML = messages[range];
+    document.getElementById("pic").src = pictures[range];
+    document.getElementById("num-crt").innerHTML = "you got " + correct + " correct";
+    document.getElementById("after-finish").style.visibility = "visible";
+}
